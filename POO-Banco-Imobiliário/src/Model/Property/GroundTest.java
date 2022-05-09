520d9f661afcec8c0438efecfb5bc97908453c59
Loki <80011623+ArthLoki@users.noreply.github.com>
2022-05-08 21:33:21 -0300

    Update to send

--- POO-Banco-Imobiliário/src/Model/Property/GroundTest.java
+++ POO-Banco-Imobiliário/src/Model/Property/GroundTest.java
@@ -1,5 +1,7 @@
 package Model.Property;
 
+import java.awt.Color;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -10,7 +12,8 @@ public class GroundTest {
 	@Test(timeout= TIMEOUT)
 	public void testConstructor() {
 		int aluguel[]= {2, 5, 10};
-		Ground g = new Ground("Lagoa", -1, aluguel, 100);
+		int price[] = {100, 200, 300};
+		Ground g = new Ground("Lagoa", -1, aluguel, price, Color.green);
 		assertNotNull("criação de Ground falhou", g);
 	}
 
@@ -18,7 +21,8 @@ public class GroundTest {
 	@Test(timeout= TIMEOUT)
 	public void testGetNome() {
 		int aluguel[]= {2, 5, 10};
-		Ground g = new Ground("Lagoa", -1, aluguel, 100);
+		int price[] = {100, 200, 300};
+		Ground g = new Ground("Lagoa", -1, aluguel, price, Color.green);
 		String expected = "Lagoa";
 		String actual = g.getNome();
 		assertNotNull("instaciação do nome do terreno falhou", actual);
@@ -28,7 +32,8 @@ public class GroundTest {
 	@Test(timeout= TIMEOUT)
 	public void testGetProprietario() {
 		int aluguel[]= {2, 5, 10};
-		Ground g = new Ground("Lagoa", -1, aluguel, 100);
+		int price[] = {100, 200, 300};
+		Ground g = new Ground("Lagoa", -1, aluguel, price, Color.green);
 		int expected = -1;
 		int actual = g.getProprietario();
 		assertNotNull("instaciação do proprietário do terreno falhou", actual);
@@ -38,7 +43,8 @@ public class GroundTest {
 	@Test(timeout= TIMEOUT)
 	public void testGetAluguel() {
 		int aluguel[]= {2, 5, 10};
-		Ground g = new Ground("Lagoa", -1, aluguel, 100);
+		int price[] = {100, 200, 300};
+		Ground g = new Ground("Lagoa", -1, aluguel, price, Color.green);
 		int[] expected = {2, 5, 10};
 		int[] actual = g.getAluguel();
 		assertNotNull("instaciação do aluguel do terreno falhou", actual);
@@ -50,7 +56,8 @@ public class GroundTest {
 	@Test(timeout= TIMEOUT)
 	public void testGetValorCompra() {
 		int aluguel[]= {2, 5, 10};
-		Ground g = new Ground("Lagoa", -1, aluguel, 100);
+		int price[] = {100, 200, 300};
+		Ground g = new Ground("Lagoa", -1, aluguel, price, Color.green);
 		int expected = 100;
 		int actual = g.getValorCompra();
 		assertNotNull("instaciação do valor do terreno falhou", actual);
