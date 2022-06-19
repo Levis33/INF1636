891dd2fd4f1c4306885b4c27957be82a8319bb15
Levis33 <39637793+Levis33@users.noreply.github.com>
2022-06-19 14:02:23 -0300

    retirando coisas da main

--- POO-Banco-Imobiliário/src/Main/Main.java
+++ POO-Banco-Imobiliário/src/Main/Main.java
@@ -11,27 +11,11 @@ public class Main {
 	public static void main(String[] args) {
 		CtrlRegras match = new CtrlRegras();
 
-//		Property[] propriedade = CriaPropriedades.cria();
-
-		//////// Frames///////////////
 
 		GameIntro f2 = new GameIntro("Banco Imobiliario");
 		f2.setVisible(true);
 		f2.repaint();
 
-		// Frame f= new Frame("Banco Imobiliario", 6);
-		// f.setVisible(true);
-
-		/////////////////////////////////////
-
-		Property[] propriedades = CriaPropriedades.cria();
-
-		for (int i = 0; i < propriedades.length; i++) {
-			System.out.println(propriedades[i].getNome());
-			System.out.println(propriedades[i].getValorCompra());
-			System.out.println(i);
-
-		}
 
 	}
 
